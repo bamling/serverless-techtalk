cb1876265d07b708690cd0a714d06fd8dec289e3
bamlin <b.amling@tarent.de>
2018-09-13 11:18:57 +0200

    expanded on the http/status function to show more of what's possible with lambda proxy

--- aws-java-example/src/main/java/example/faas/aws/http/Handler.java
+++ aws-java-example/src/main/java/example/faas/aws/http/Handler.java
@@ -1,24 +1,38 @@
 package example.faas.aws.http;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.Map;
 
 public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
-    private static final Logger LOG = LogManager.getLogger(Handler.class);
-
     @Override
     public ApiGatewayResponse handleRequest(final Map<String, Object> input, final Context context) {
-        LOG.info("received: {}", input);
+        final LambdaLogger logger = context.getLogger();
 
-        final Response responseBody = new Response("This is the response message", input);
-        return ApiGatewayResponse.builder()
-                .setStatusCode(400)
-                .setObjectBody(responseBody)
-                .build();
+        try {
+            // extract body from input
+            final JsonNode body = new ObjectMapper().readTree((String) input.get("body"));
+            logger.log("Hello " + body.get("name").asText());
+
+            final Response responseBody = new Response("this is the response message", input);
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(200)
+                    .setObjectBody(responseBody)
+                    .build();
+
+        } catch (final Exception e) {
+            logger.log("Got exception: " + e.getMessage());
+
+            final Response responseBody = new Response("something bad happened", input);
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(500)
+                    .setObjectBody(responseBody)
+                    .build();
+        }
     }
 }
